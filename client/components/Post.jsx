cd6428f44914f63675c8aee404b2067a3f1c9e64
Ben Childs <benjamin.t.childs@gmail.com>
2017-07-31 19:56:46 -0500

    images appear in posts

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -4,15 +4,19 @@ class Post extends React.Component {
 	constructor(props) {
 		super(props)
 
-
+		this.image = new Image();
+		this.image.src = props.post.imagePath;
 	}
 
+	componentDidMount() {
+
+	}
 
 	render() {
 		return (
 			<div className="doggo-post">
-				<p>{props.username}</p>
-				<img src={props.imageUrl} />
+				<p>{this.props.post.username}</p>
+				<img src={this.image.src}/>
 			</div>
 		);
 	}
--- client/components/PostPanel.jsx
+++ client/components/PostPanel.jsx
@@ -5,7 +5,9 @@ import Post from './Post.jsx';
 const PostPanel = (props) => {
 	return(
 		<div className="post-list">
-			
+			{props.posts.map(post => 
+				<Post post={post} key={post._id}/>
+			)}
 		</div>
 	);
 }
